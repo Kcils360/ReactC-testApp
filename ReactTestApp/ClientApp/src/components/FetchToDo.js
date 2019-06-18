065b2435032747e5472b81f3fb65627d86fe4c29
Gregory Dukes <kcils360@gmail.com>
2019-06-18 12:57:03 -0700

    adding files to git

---
+++ ReactTestApp/ClientApp/src/components/FetchToDo.js
@@ -0,0 +1,54 @@
+﻿import React, { Component } from 'react';
+
+export class FetchToDo extends Component {
+    static displayname = FetchToDo.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { todos: [], loading: true };
+
+        fetch('api/ToDoApp/Todos')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ todos: data, loading: false });
+            });
+    }
+
+    static renderToDosTable(todos) {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Task Name</th>
+                        <th>Description</th>
+                        <th>Done</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo =>
+                        <tr key={todo.TaskName}>
+                            <td>{todo.TaskName}</td>
+                            <td>{todo.TaskDescription}</td>
+                            <td><input type="checkbox" /></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : FetchToDo.renderToDosTable(this.state.todos);
+
+        return (
+            <div>
+                <h1>Some Tasks To Do</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+}
+
---
+++ ReactTestApp/ClientApp/src/components/ToDoApp.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+
+export class ToDoApp extends Component {
+    static displayName = ToDoApp.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        };
+        this.saveToDo = this.saveToDo.bind(this);
+    }
+
+    saveToDo() {
+        this.setState({
+            number: this.state.number + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>To Do App</h1>
+
+                <p>Type Here</p>
+                <input type="text" />
+                <button className="btn btn-primary" onClick={this.saveToDo}>Save</button>
+
+                <p>Number of ToDos: {this.state.number}</p>
+            </div>
+
+        );
+    }
+}
